39edeff7cd3430db34611502a668d73c560e52ef
Ktiktok <ktiktok96@gmail.com>
2021-11-22 11:30:35 -0500

    Replaced with child <div> & added QuizQuestion
    Removed the text Quiz and replaced it with a child<div> that has a className of QuizQuestion

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component{
   
   render() {
     return ( 
-      <div>Quiz</div>
+      <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
       )
   } 
 }
