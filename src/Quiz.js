4f402aa6606f28f15c5a83d39239cc70a1ccde1d
Ktiktok <ktiktok96@gmail.com>
2021-11-22 21:14:47 -0500

    adding a prop to the QuizQuestion component

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,8 @@ class Quiz extends Component{
     this.state= {quiz_position: 1}
   }
   
-  render() {
+  render(quiz_question) {
+    quiz_question = quizData.quiz_questions(quiz_position - 1)
     return ( 
       <div><QuizQuestion /></div>
       )
